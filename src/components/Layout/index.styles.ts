f10153bc6be0bbe282e5ec41c85a7027b1678b6e
bgldjrtxmn <94174867+bgldjrtxmn@users.noreply.github.com>
2023-03-21 14:58:26 +0300

    feat(LayoutAndInput): Implement Layout and Input

---
+++ src/components/Layout/index.styles.ts
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+export let Wrapper = styled.div`
+  display: grid;
+  grid-gap: 25px;
+  background-color: #f9f7f7;
+  border-radius: 20px;
+  padding: 20px 52px 85px;
+`;
+
+export let Header = styled.div`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 40px;
+  line-height: 1.1;
+  color: #112d4e;
+`;
